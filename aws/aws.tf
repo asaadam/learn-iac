612af21afa25d6dc4b1d48a8a9c5234da3398a64
Adam <graha.asa@gmail.com>
2020-08-26 15:03:09 +0700

    add backend

--- aws/aws.tf
+++ aws/aws.tf
@@ -1,9 +1,19 @@
-# provider "aws" {
-#   profile = "default"
-#   region  = "ap-southeast-1"
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terrafom-backend"
+    key            = "demo"
+    dynamodb_table = "terraform-lock"
+    region         = "ap-southeast-1"
+  }
+}
 
-# resource "aws_instance" "example" {
-#   ami           = "ami-09a4a9ce71ff3f20b"
-#   instance_type = "t2.nano"
-# }
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-09a4a9ce71ff3f20b"
+  instance_type = "t2.nano"
+}
