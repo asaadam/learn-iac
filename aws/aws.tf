9ce4c9813affc26928c18675a37b1aa216628468
Adam <graha.asa@gmail.com>
2020-08-26 10:31:02 +0700

    destory

--- aws/aws.tf
+++ aws/aws.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  profile = "default"
-  region  = "ap-southeast-1"
-}
+# provider "aws" {
+#   profile = "default"
+#   region  = "ap-southeast-1"
+# }
 
-resource "aws_instance" "example" {
-  ami           = "ami-09a4a9ce71ff3f20b"
-  instance_type = "t2.nano"
-}
+# resource "aws_instance" "example" {
+#   ami           = "ami-09a4a9ce71ff3f20b"
+#   instance_type = "t2.nano"
+# }
